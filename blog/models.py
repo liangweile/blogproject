96139d7cd97dd4645ed28e18f00f52436f4a7c4f
liangweile <519936974@qq.com>
2018-05-08 08:15:05 +0800

    add excerpt

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+import markdown 
+from django.utils.html import strip_tags
 
 # Create your models here.
 
@@ -23,9 +25,22 @@ class Post(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE,)
     tags = models.ManyToManyField(Tag, blank=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE,)
+    views = models.PositiveIntegerField(default=0)
     def __str__(self):
         return self.title
     def get_absolute_url(self):
         return reverse('detail', kwargs={'pk': self.pk})
+    def increase_views(self):
+        self.views += 1
+        self.save(update_fields=['views'])
+    def save(self, *args, **kwargs):
+        if not self.excerpt:
+            md = markdown.Markdown(extensions=[
+                'markdown.extensions.extra',
+                'markdown.extensions.codehilite',
+                ])
+            self.excerpt = strip_tags(md.convert(self.body))[:54]
+        super(Post, self).save(*args, **kwargs)
     class Meta:
         ordering = ['-create_time']
+
--- blogproject/urls.py
+++ blogproject/urls.py
@@ -18,10 +18,15 @@ from django.contrib import admin
 from django.views.static import serve
 from django.conf import settings
 
-if not settings.DEBUG:
-	urlpatterns = [
-	    url(r'^admin/', admin.site.urls),
-	    url(r'', include('blog.urls')),
-	    url(r'', include('comments.urls')),
-	    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
+#if not settings.DEBUG:
+#	urlpatterns = [
+#	    url(r'^admin/', admin.site.urls),
+#	    url(r'', include('blog.urls')),
+#	    url(r'', include('comments.urls')),
+#	    url(r'^static/(?P<path>.*)$', serve,{'document_root':settings.STATIC_ROOT}),
+#]
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'',include('blog.urls')),
+    url(r'',include('comments.urls')),
 ]
